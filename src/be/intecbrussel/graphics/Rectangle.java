dd4345d751d7049c1c9e979b70bab3edc524f0e3
IzunaTan <manuel.cogneau@intecbrussel.be>
2022-04-08 11:51:38 +0200

    Exercises are fun, as long as it is not outside

---
+++ src/be/intecbrussel/graphics/Rectangle.java
@@ -0,0 +1,70 @@
+package be.intecbrussel.graphics;
+
+
+public class Rectangle{
+    private int height;
+    private int width;
+    private int x;
+    private int y;
+
+    public void setHeight(int height) {
+        if (height < 0) {
+            this.height = -height;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public void setWidth(int width) {
+        this.width = Math.abs(width);
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getArea() {
+        return height * width;
+    }
+
+    public int getPerimeter() {
+        return doubleHeight() + doubleWidth();
+    }
+
+    private int doubleHeight() {
+        return 2 * height;
+    }
+
+    private int doubleWidth() {
+        return 2 * width;
+    }
+
+    public void grow(int d) {
+        if (d > 0) {
+            this.height += d;
+            this.width += d;
+        }
+    }
+
+
+}
